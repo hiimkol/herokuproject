e8b64e167dff8adf1b916fbd4054c57d5350bf20
Krader <hiimkol@gmail.com>
2018-05-02 15:00:46 +0700

    fix typing error: food

--- src/main/java/com/krader/herokuproject/FoodPalace.java
+++ src/main/java/com/krader/herokuproject/FoodPalace.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "employee")
+@XmlRootElement(name = "food")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class FoodPalace {
 	String id, name, address, phone;
